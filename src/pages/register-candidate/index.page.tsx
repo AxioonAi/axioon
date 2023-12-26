44ecc9e916a19788661a680328b1402fac63e45f
Daniel Carling <117768647+danielcarling@users.noreply.github.com>
2023-12-26 11:03:33 -0400

    update

--- src/pages/register-candidate/index.page.tsx
+++ src/pages/register-candidate/index.page.tsx
@@ -3,7 +3,10 @@ import { RegisterAccountHeader } from "@/components/register-account/Header";
 import { useState } from "react";
 import {
   Container,
-  Main
+  InstructionStep,
+  Instructions,
+  Main,
+  StepName,
 } from "./styles";
 
 export default function RegisterCandidate() {
@@ -16,7 +19,44 @@ export default function RegisterCandidate() {
     <Container>
       <RegisterAccountHeader type="dark" />
       <Main>
-        
+        <Instructions>
+          <InstructionStep className="step1">
+            <img src="/register-candidate/1.png" alt="" />
+            <div className="description">
+              <StepName>Inserir Dados</StepName>
+              <p>
+                Ao inserir os dados nossa tecnologia será capaz de analisar.
+              </p>
+            </div>
+          </InstructionStep>
+          <InstructionStep className="step2">
+            <img src="/register-candidate/2.png" alt="" />
+            <div className="description">
+              <StepName>Inserir Dados</StepName>
+              <p>
+                Ao inserir os dados nossa tecnologia será capaz de analisar.
+              </p>
+            </div>
+          </InstructionStep>
+          <InstructionStep className="step3">
+            <img src="/register-candidate/3.png" alt="" />
+            <div className="description">
+              <StepName>Inserir Dados</StepName>
+              <p>
+                Ao inserir os dados nossa tecnologia será capaz de analisar.
+              </p>
+            </div>
+          </InstructionStep>
+          <InstructionStep className="step4">
+            <img src="/register-candidate/4.png" alt="" />
+            <div className="description">
+              <StepName>Inserir Dados</StepName>
+              <p>
+                Ao inserir os dados nossa tecnologia será capaz de analisar.
+              </p>
+            </div>
+          </InstructionStep>
+        </Instructions>
       </Main>
       <Footer type="dark" />
     </Container>
--- src/pages/register-candidate/styles.ts
+++ src/pages/register-candidate/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   position: relative;
   padding-bottom: 4rem;
   min-height: 100vh;
-  background: linear-gradient(117deg, #0D123C 0%, #34374C 100%);
+  background: linear-gradient(117deg, #0d123c 0%, #34374c 100%);
 
   @media (max-width: 768px) {
     padding-bottom: 8rem;
@@ -14,10 +14,66 @@ export const Container = styled.div`
 export const Main = styled.main`
   display: flex;
   justify-content: space-between;
-  max-width: 1440px;
+  max-width: 1200px;
   margin: auto;
 
   @media (max-width: 1180px) {
-    flex-direction: column-reverse;
   }
+`;
+
+export const Instructions = styled.div`
+  display: grid;
+  grid-template-columns: 90% 90%;
+  grid-template-areas:
+    'step2 step4'
+    'step1 step3';
+
+
+  .step1 {
+    grid-area: step1;
+  }
+  .step2 {
+    grid-area: step2;
+    justify-self: end;
+
+  }
+  .step3 {
+    grid-area: step3;
+  }
+  .step4 {
+    grid-area: step4;
+    justify-self: end;
+
+  }
+`;
+
+export const InstructionStep = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 10px;
+
+  .description {
+    width: 211px;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  p {
+    margin: 0;
+    color: #fff;
+    font-weight: 500;
+  }
+`;
+
+export const StepName = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.25rem;
+  font-weight: 600;
+  background-color: #fff;
+  border-radius: 30px;
+  width: 211px;
+  height: 50px;
 `;
