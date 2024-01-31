781a4544aadfc1a36b3746c738be376f0323a930
VictorHOgawa <victor.ogawa@gmail.com>
2024-01-31 14:34:37 -0400

    throw new error

--- src/pages/api/maps.api.tsx
+++ src/pages/api/maps.api.tsx
@@ -7,8 +7,13 @@ export default async function handler(
 ) {
   const city = req.query.city as string;
   const state = req.query.state as string;
-  const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${city}%20${state}&region=BR&key=${process.env.GOOGLE_MAPS_API_KEY}`,
-  );
+  const response = await axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${city}%20${state}&region=BR&key=${process.env.GOOGLE_MAPS_API_KEY}`,
+    )
+    .then((res) => res)
+    .catch((err) => {
+      throw new Error(err.message);
+    });
   res.status(200).json(response.data);
 }
